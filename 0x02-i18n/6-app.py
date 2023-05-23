63ad52330d0b655b8df591b5fc2423937fc36ee0
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-05-24 02:30:40 +0300

    i18n

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" module 5-app.py with a basic Flask app and Babel setup """
+""" module 6-app.py with a basic Flask app and Babel setup """
 
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
@@ -59,7 +59,7 @@ def before_request():
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """ Basic Flask App """
-    return render_template('5-index.html')
+    return render_template('6-index.html')
 
 
 if __name__ == '__main__':
--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" module 5-app.py with a basic Flask app and Babel setup """
+""" module 7-app.py with a basic Flask app and Babel setup """
 
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
